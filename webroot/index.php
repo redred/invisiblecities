c59d121ee820fc60273ee4c53b936ee5eb904dd2
Red de Leon <red@red-deleon.com>
2015-02-01 16:09:49 -0500

    Add base JS files

---
+++ webroot/index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+  <meta charset="utf-8">
+  <title>Invisible Cities</title>
+  <script data-main="js/compiled/require-config" src="js/lib/require/require.js"></script>
+  <link rel="stylesheet" type="text/css" href="css/compiled/style.css">
+  <script>
+  </script>
+  <style type="text/css">
+  </style>
+<body class="home">
+<div class="page">
+  <header>
+    <h2>Invisible Cities</h2>
+  </header>
+  <section class="body">
+    
+  </section>
+  <footer>
+    &copy; 2015 Footer
+  </footer>
+</div>
+</body>
+</html>
